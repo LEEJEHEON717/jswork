cf7bb8cf2f003758fc87e24c12b9854fbbd490f1
LEEJEHEON717 <leejehun0717@naver.com>
2023-11-13 18:29:00 +0900

    Date, setlnterval

--- day3/js/array1.js
+++ day3/js/array1.js
@@ -1,5 +1,5 @@
-// 배열 사용
-// 순서가 0번부터 시작함
+//배열 사용
+//순서가 0번부터 시작함
 let animal = []; //빈 배열 생성
 console.log(animal);
 console.log(animal.length + "개");
@@ -11,7 +11,7 @@ animal[2] = 'horse';
 animal[3] = 'cat';
 console.log(animal);
 
-//특정요소(값) 조회(검색)
+//특정 요소(값)를 조회(검색)
 console.log(animal[1]);
 
 //수정(변경)
@@ -20,9 +20,14 @@ animal[2] = "dog";
 //삭제
 delete animal[1];
 
-//전체요소(값) 조회(목록)
+//전체 요소(값) 조회(목록)
 //0번 인덱스부터 시작하고, 배열이 길이 미만일 것
 for(let i = 0; i < animal.length; i++){
+  document.write(animal[i] + " ");
+}
+document.write("<br>");
+//for in 문
+for(let i in animal){
   document.write(animal[i] + " ");
 }
 
--- day3/js/array2.js
+++ day3/js/array2.js
@@ -1,5 +1,5 @@
 //Array 객체를 사용한 배열
-let num = new Array(); //빈 배열 생성
+let num = new Array();  //빈 배열 생성
 console.log(num);
 console.log(num.length + "개");
 
@@ -14,28 +14,34 @@ console.log(num);
 console.log(num[0]);
 
 //수정
-
+num[1] = 40;
 
 //전체 요소 검색
 for(let i=0; i<num.length; i++){
-console.log(num[i]);
+  console.log(num[i]);
+}
+
+//for in문 사용
+/*for(let 변수 in 배열이름){
+  console.log(배열이름[변수])
+}*/
+console.log("for ~ in문 검색");
+for(let n in num){
+  console.log(num[n]);
 }
 
 //연산
+console.log("인덱스로 연산하기");
 console.log(num[1] - num[2]);
 console.log(num[1] <= num[2]);
 
 //합계 및 평균
 let sum = 0;
-for(let i=0; i<num.length; i++) {
-  sum = sum + num[i];
-  sum += num[i]; //누적 합계
+let avg;
+for(let i = 0; i < num.length; i++){
+  //sum = sum + num[i];
+  sum += num[i];  //누적 합계
 }
-avg = sum / num.length; //평균 = 합계 / 개수
+avg = sum / num.length;  //평균 = 합계 / 개수
 console.log(sum);
 console.log(avg.toFixed(2));
-
-
-
-
-
